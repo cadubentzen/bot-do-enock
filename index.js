f9177b7f33c3d82e8616d8262d19567b9b22063e
cadubentzen <cadubentzen@gmail.com>
2018-02-06 17:26:12 -0300

    - Add "oh yes" or "oh no" answers - Fix bug for only replying when Enock's message has M replaced by B

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Telegraf = require('telegraf');
+const Extra = require('telegraf/extra');
 
 const { convertMtoB } = require('./util');
 
@@ -7,8 +8,20 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start(ctx => ctx.reply('Welcobe!'));
 
 bot.on('message', (ctx) => {
-  if (ctx.from.id === parseInt(process.env.ENOCK_ID, 10)) {
-    ctx.reply(convertMtoB(ctx.message.text));
+  if (!('text' in ctx.message)) {
+    return;
+  }
+  if (ctx.message.text.includes('?')) {
+    ctx.reply(
+      Math.random() > 0.5 ? 'Oh yes!' : 'Oh no!',
+      Extra.inReplyTo(ctx.message.message_id) // eslint-disable-line
+    );
+  } else if (ctx.from.id === parseInt(process.env.ENOCK_ID, 10)
+             && ctx.message.text.includes('m')) {
+    const converted = convertMtoB(ctx.message.text);
+    if (converted !== ctx.message.text) {
+      ctx.reply(converted);
+    }
   }
 });
 
